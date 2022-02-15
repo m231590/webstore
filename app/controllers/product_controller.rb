0233af1ae5d91207192ae6f17ec5880249235d52
Charles Doherty <m231590@usna.edu>
2022-02-15 00:05:17 -0500

    Worked on CSS

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,4 +2,30 @@ class ProductController < ApplicationController
     def show
         @products = Product.find(params[:id])
     end
+    
+    def new
+        @products = Product.new
+    end
+
+    def create
+        @products = Product.new(name: "...", description: "...", price: "...")
+
+        if @products.save
+            redirect_to @products
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @products = Product.find(params[:id])
+        @products.destroy
+
+        redirect_to root_path, status: :see_other
+    end
+
+    private
+        def product_params
+            params.require(:product).permit(:title, :body)
+        end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     validates :name, presence: true
+    validates :description, presence: true, length: {minimum: 10}
     validates :price, comparison: {greater_than: 0}
 end
