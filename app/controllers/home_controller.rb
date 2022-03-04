b167fec6adee86f30e9f53d94155da3426259a0c
Charles Doherty <m231590@usna.edu>
2022-03-03 23:09:56 -0500

    added cart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,23 @@
 class HomeController < ApplicationController
     def index
-        @products = Product.all
+        if session[:cart].nil?
+            session[:cart] = []
+        end
+        @products = Product.all #pass all the products to the index view
+        @cart = session[:cart] #pass cart information to the index view
+    end
+
+    def buy
+        if session[:cart].nil?
+            session[:cart] = []
+        end
+        product = Product.find(params[:id]) #get id for the parameter you want to buy
+        session[:cart].append(product) #cart cookie
+        redirect_to :root #no new view for the buy function
+    end
+
+    def checkout
+        @cart = session[:cart]
+        session[:cart] = [] #empty the cart
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  get "/products/:id/buy", to: "home#buy", as: "buy"
+  get "/checkout", to: "home#checkout", as: "checkout"
   resources :product
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
